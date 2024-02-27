c6aef3304b63b07275304cba6a78b94d68eefe88
khaoula <khaoulachetioui4@gmail.com>
2024-02-27 15:57:57 +0100

    new version

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  //process.env.DB_URL,
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+    process.env.DBCONNECTIONSTRING,
+  //process.env.DB_NAME, // Database name
+  //process.env.DB_USER, // User
+  //process.env.DB_PASS, // Password
   {
-    host: process.env.DB_HOST, 
+   // host: process.env.DB_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
